e9471bae0bb8cdaa7d9ab400c9e3225989d33623
jeemyeong <jeemyeong@gmail.com>
2016-07-19 12:08:56 +0000

    set for credential in s3

--- config/initializers/aws_s3.rb
+++
@@ -1,9 +0,0 @@
-CarrierWave.configure do |config|
-  config.fog_credentials = {
-      :provider               => 'AWS',
-      :aws_access_key_id      => '',
-      :aws_secret_access_key  => '',
-      :region                 => 'ap-northeast-2'
-  }
-  config.fog_directory  = 'jeemyeongrails'
-end
