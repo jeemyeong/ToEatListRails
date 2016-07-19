f0457d7d6192b05377b3a6bed4d322a371443f99
jeemyeong <jeemyeong@gmail.com>
2016-07-19 11:50:44 +0000

    set credentials for s3(2)

--- config/initializers/aws_s3.rb
+++ config/initializers/aws_s3.rb
@@ -1,7 +1,7 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
       :provider               => 'AWS',
-      :aws_access_key_id      => 'AKIAJZJTXDS4ZLAWZKSA',
+      :aws_access_key_id      => '',
       :aws_secret_access_key  => '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',
       :region                 => 'ap-northeast-2'
   }
